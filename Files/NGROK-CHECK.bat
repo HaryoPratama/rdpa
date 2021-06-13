cf4ba8cb6677ffd4f51448b976c442e514dff826
NotLucas <62017665+HaryoPratama@users.noreply.github.com>
2021-06-13 11:25:49 +0700

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -9,11 +9,11 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure TOKEN is correct in your GitHub repository Settings> Secrets> Repository secret. Or it's can happen if your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: admin
 echo Pass: @FreeRDP123#
-curl -O https://raw.githubusercontent.com/NflaZ/rdp/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/NflaZ/rdp/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\npp.7.9.4.Installer.x64.exe" https://raw.githubusercontent.com/NflaZ/rdp/main/Files/npp.7.9.4.Installer.x64.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Everything.exe" https://raw.githubusercontent.com/NflaZ/rdp/main/Files/Everything.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\BANDIZIP-SETUP.exe" https://raw.githubusercontent.com/NflaZ/rdp/main/Files/BANDIZIP-SETUP.exe > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/HaryoPratama/rdpa/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/HaryoPratama/rdpa/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\npp.7.9.4.Installer.x64.exe" https://raw.githubusercontent.com/HaryoPratama/rdpa/main/Files/npp.7.9.4.Installer.x64.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Everything.exe" https://raw.githubusercontent.com/HaryoPratama/rdpa/main/Files/Everything.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\BANDIZIP-SETUP.exe" https://raw.githubusercontent.com/HaryoPratama/rdpa/main/Files/BANDIZIP-SETUP.exe > out.txt 2>&1
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
